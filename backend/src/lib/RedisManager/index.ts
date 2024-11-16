bfb390a7221aa5fa61eb265fe2557e4af8c8360d
{Suksham-sharma} <sukshamever@gmail.com>
2024-11-16 23:28:43 +0530

    removed redundant initializations

--- backend/src/lib/RedisManager/index.ts
+++ backend/src/lib/RedisManager/index.ts
@@ -5,12 +5,9 @@ const URL = process.env.REDIS_URL || "redis://localhost:6379";
 export class RedisManager {
   private static instance: RedisManager;
   private queueClient: RedisClientType;
-  private subscribeClient: RedisClientType;
   private constructor() {
     this.queueClient = createClient({ url: URL });
     this.queueClient.connect();
-    this.subscribeClient = createClient({ url: URL });
-    this.subscribeClient.connect();
   }
 
   public static getInstance() {

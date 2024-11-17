40a0f6b01368c4a86b15c01af0f80d52370d3780
{Suksham-sharma} <sukshamever@gmail.com>
2024-11-17 06:51:38 +0530

    ws server init + config

---
+++ ws-server/src/index.ts
@@ -0,0 +1,35 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+import { processRequests } from "./lib/utils";
+
+const server = http.createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws: WebSocket) => {
+  console.log(`${new Date().toISOString()} New client connected`);
+  ws.on("error", console.error);
+
+  ws.on("message", (message) => {
+    // handle incoming message
+
+    console.log(`Recieved message`, message);
+  });
+
+  ws.on("close", () => {
+    // handleConnectionClosed(ws);
+  });
+});
+
+async function startServer() {
+  try {
+  } catch (error: any) {
+    server.listen(8080, () => {
+      console.log("Server started at Port 8080 ... ");
+    });
+    console.log("Error while starting the server");
+  }
+
+  await processRequests();
+}
+
+startServer();
